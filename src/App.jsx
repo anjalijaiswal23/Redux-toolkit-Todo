a2d8282a707a6bd1743d5de4f84829cf9456c55c
w3villa-anjali-jaiswal <anjali.jaiswal@w3villa.com>
2024-07-14 12:44:46 +0530

    redux_learning

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AddTodo from './Components/Addtodo'
+import Todos from './Components/Todos'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
